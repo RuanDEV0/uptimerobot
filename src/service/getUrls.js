a9975418e6feaafc9240cffeadc8c09014136044
Ruan <oliveiraruan2018@gmail.com>
2026-02-12 11:49:58 -0400

    feat: simplify URL data retrieval by exporting raw environment variable

--- src/service/getUrls.js
+++ src/service/getUrls.js
@@ -1,13 +1,7 @@
-import logger from "../logger/logger.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const data = process.env.URLS;
-const urls = [];
 
-for (const url of data.split(",")) {
-    urls.push(url.trim());
-}
-
-export default urls;
+export default data;
