2e14b8ce9eb2dd53ef32b79e6f22664c07b933c5
Ruan <oliveiraruan2018@gmail.com>
2026-02-12 10:37:24 -0400

    feat: update server port configuration and enhance app logging

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import { pinoHttp } from "pino-http";
 import logger from "./logger/logger.js";
+// import './service/pingUrl.js'
+// import sendEmail from "./service/sendEmail.js";
+// import { pingUrl } from "./service/pingUrl.js";
 
 const app = express();
 
-app.use(pinoHttp(logger));
+app.use(pinoHttp({ logger}));
 
 app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send('Server is running');
+});
 
 export { app };
--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 import { app } from "./app.js";
 import logger from "./logger/logger.js";
-import "./service/index.js";
-import "dotenv";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT_SERVER || 3000;
 
 app.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
